5dbbe73c8d28a7107a6b378e01b9f7619da5f24e
Michavgust <michavgust19@gmail.com>
2025-02-02 20:30:11 +0300

    Небольшие исправления

--- script/main.js
+++ script/main.js
@@ -4,3 +4,11 @@ document.querySelector('.btn-watch-video').addEventListener('click', function() 
     video.style.display = 'block';
     video.play();
 });
+
+
+// const discounts = document.querySelectorAll('.discount');
+// let percents = [];
+// for (let discount in discounts) {
+//     let percent= parseInt(discount.textContent);
+//     discount.style.background = percent <= -30 ? '#FF5BC6' : '#18C576';
+// }
